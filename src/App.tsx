7347f2c1f107513a0e4b6cdb6d8c82e3f82d80c7
Praisebuka <praise.njoga@gmail.com>
2025-10-14 10:18:12 +0100

    auth setup & protected routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,34 @@
 import { Suspense } from "react";
-import { useRoutes, Routes, Route } from "react-router-dom";
-import Home from "./components/home";
+import { useRoutes, Routes, Route, Navigate, Outlet } from "react-router-dom";
+import Dashboard from "./components/dashboard";
 import routes from "tempo-routes";
+import { getActiveUser } from "./hooks/localstorage";
+import Login from "./components/login/login.page";
+import Register from "./components/register/register.page";
 
 function App() {
   return (
     <Suspense fallback={<p>Loading...</p>}>
       <>
         <Routes>
-          <Route path="/dashboard" element={<Home />} />
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+
+          <Route path="/" element={<PrivateRoute />}>
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Route>
         </Routes>
         {import.meta.env.VITE_TEMPO === "true" && useRoutes(routes)}
       </>
     </Suspense>
   );
 }
+
+const PrivateRoute = () => {
+  var activeUser = getActiveUser();
+  if (activeUser == null) return <Navigate to="/login" />;
+  return <Outlet />;
+};
 
 export default App;
