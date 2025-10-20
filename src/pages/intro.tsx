4638fe759a7e309f60f6d8561547d1b0a6d2a25a
Praisebuka <praise.njoga@gmail.com>
2025-10-20 20:37:55 +0100

    appropriate linking

--- src/pages/intro.tsx
+++ src/pages/intro.tsx
@@ -4,7 +4,7 @@ const IntroSection = () => (
     <div className="intro-container wow fadeIn">
       <h1 className="mb-4 pb-0"> Cybersecurity for <br /> <span> Small </span> & <span> Medium </span> Enterprises </h1>
       <p className="mb-4 pb-0"> Built for SMEs. Watch our tutorial video on how to get started </p>
-      <a href={'https://www.youtube.com/watch?v=av5JD1dfj_c'} className="venobox play-btn mb-4" data-vbtype="video" data-autoplay="true"></a>
+      <a href={'https://www.youtube.com/watch?v=tdLUlTP5TKo'} className="venobox play-btn mb-4" data-vbtype="video" data-autoplay="true" target="_blank"></a>
 
       <a href="#about" className="about-btn scrollto"> About AfriGuard </a>
     </div>
